d4d0dcb5c2cb1a8c88141a2e3f834d877d09d9b0
FlorianMorel80 <you@example.com>
2021-01-05 16:58:01 +0100

    ça avance

--- partie-10-tp1/index2.php
+++ partie-10-tp1/index2.php
@@ -1,28 +1,60 @@
 <?php 
 $error = array(); 
 
-
-if (isset($_POST['inputName'])){
-    // Tester si le champ existe
-}
-
-    if (empty($_POST["inputName"])) {
-        array_push($error, 'Champs requis');
-    } else {
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$inputName)) 
-            array_push($error, 'Seuls les lettres et les espaces sont autorisés');
-    }   
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($error)) {
-        // Afficher les données 
+// Si le formulaire est envoyé
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    
+    // Si le champs existe
+    if (isset($_POST['inputName'])) {
+        $inputName = $_POST['inputName'];
         
-        echo $inputName;
+        // Si le champs est vide
+        if (empty($inputName)) {
+            $error['inputName'] = 'Champs requis';
         } else {
-        // Afficher le formulaire et/ou avec les erreurs 
-            include 'form.php';
+
+            // Si il n'est pas vide, alors je nettoie la variable 
+            $inputName = filter_var($inputName, FILTER_SANITIZE_STRING);
+
+            // Verifier : Si le champs ne correspond pas à la REGEX
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$inputName)) {
+                $error['inputName'] = 'Seuls les lettres et les espaces sont autorisés';
+            } 
+        }
+    } else {
+        $error['inputName'] = 'Problème lors de l\'envoi du formulaire';  
+    }
+
+    //chp 2
+// ***************FirstName**********************
+if (isset($_POST['inputFirstName'])) {
+    $inputFirstName = $_POST['inputFirstName'];
+      if (empty($inputFirstName)) {
+        $error['inputFirstName'] = '*Champs requis';
+      } else {
+        // Si il n'est pas vide, alors je nettoie la variable 
+        $inputFirstName = filter_var($inputFirstName, FILTER_SANITIZE_STRING);
+        // Verifier : Si le champs ne correspond pas à la REGEX
+        if (!preg_match("/^[A-Za-z-' ]*$/",$inputFirstName)) {
+            $error['inputFirstName'] = '*Seuls les lettres et les espaces sont autorisés';
+        } 
+    }
+  } else {
+      $error['inputFirstName'] = '*Problème lors de l\'envoi du formulaire';  
+  }
+
 }
 
-
+ // Si le champs est remplit et qu'il n'y a pas d'erreurs
+if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($error)) {
+    // Afficher les données 
+    echo $inputName . '<br/>';
+    echo $inputFirstName;
+    
+} else {
+    // Afficher le formulaire et/ou avec les erreurs 
+    include 'form.php';
+}
 ?>
 
 <!DOCTYPE html>
